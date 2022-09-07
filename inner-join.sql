3a0ab4f8b8c542a901bffadea1dc4490949299cc
Emre Tarhan <hasan35@protonmail.com>
2022-09-07 04:24:26 +0300

    Update inner-join.sql

--- inner-join.sql
+++ inner-join.sql
@@ -6,6 +6,8 @@ CREATE TABLE `items` (
   `itemBrand` varchar(120) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+
 -- Markaların listeleneceği tabloyu oluşturalım
 --
 CREATE TABLE `brands` (
@@ -13,6 +15,20 @@ CREATE TABLE `brands` (
   `brandName` varchar(130) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Tabloları birleştirelim
 
-SELECT * FROM items INNER JOIN brands ON brands.id=items.itemBrand
+
+-- Tabloları verileri ekleyelim
+-- Gördüğünüz gibi `items` tablosundaki itemBrand değeri ile `brands` tablosundaki id değeri eşleşiyor.
+-- Ürünü veritabanına eklerken de <select> etiketinin value niteliğine marka id olmalıdır. Eşleşme bu şekilde sağlanınyor.
+-- 
+INSERT INTO `items` (`id`, `itemName`, `itemBrand`) VALUES
+(1, 'iPhone 13 128GB', '1'),
+(2, 'Samsung Note 8', '2'),
+(3, 'iPhone SE 64GB', '1');
+
+
+INSERT INTO `brands` (id`, `brandName`) VALUES
+(1, 'Apple', 'Apple Türkiye'),
+(2, 'Samsung', 'Samsung Türkiye'),
+(3, 'Xiaomi', 'Xiaomi Türkiye'),
+
